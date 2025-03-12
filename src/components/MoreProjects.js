cf04b622fb2cd3dabe81b6448d8239d8b0a4cf91
Mohamed  Atwa <mohamed.872872d@gmail.com>
2025-03-12 13:24:02 +0200

    Update MoreProjects.js

--- src/components/MoreProjects.js
+++ src/components/MoreProjects.js
@@ -92,8 +92,10 @@ const MoreProjects = ({ AllProjects }) => {
                 </div>
               </div>
               <div className="project-info">
-                <h3>{project.title}</h3>
-                <p>{project.description}</p>
+                <div>
+                  <h3>{project.title}</h3>
+                  <p>{project.description}</p>
+                </div>
                 <div className="project-tech">
                   {project.tech.map((tech, i) => (
                     <span key={i} className="tech-tag">
